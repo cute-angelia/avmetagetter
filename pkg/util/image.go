20458cc5f98477cfaf5d7a09890906bf9e704f30
vanilla <rose20.99.c@gmail.com>
2021-07-02 10:18:01 +0800

    update

--- pkg/util/image.go
+++ pkg/util/image.go
@@ -101,7 +101,7 @@ func PosterCover(srcPhoto, newPhoto string, cfg *ConfigStruct) error {
 		// 获取图片边界
 		b := img.Bounds()
 		// 获取图片宽度并设置为一半
-		width = b.Max.X / 2
+		width = b.Max.X / 2 + 21
 		// 获取图片高度
 		height = b.Max.Y
 		// 将x坐标设置为0
